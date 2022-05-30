8bc4c0ea2b045c467af8dcb579125e7d64036dbd
dahuin-1 <dawhite01@naver.com>
2022-05-30 16:13:33 +0900

    backup banking

--- banking/go/banking.go
+++ banking/go/banking.go
@@ -1,1 +1,154 @@
-package _go
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type SmartContract struct {
+	contractapi.Contract
+}
+
+func (t *SmartContract) Init(ctx contractapi.TransactionContextInterface, A string, Aval int, B string, Bval int) error {
+
+	//fmt.Println("ex02 Init")
+	//_, args := stub.GetFunctionAndParameters()
+	//var A, B string    // Entities
+	//var Aval, Bval int // Asset holdings
+	//var err error
+	//
+	//if len(args) != 4 {
+	//	return shim.Error("Incorrect number of arguments. Expecting 4")
+	//}
+	//
+	//// Initialize the chaincode
+	//A = args[0]
+	//Aval, err = strconv.Atoi(args[1])
+	//if err != nil {
+	//	return shim.Error("Expecting integer value for asset holding")
+	//}
+	//B = args[2]
+	//Bval, err = strconv.Atoi(args[3])
+	//if err != nil {
+	//	return shim.Error("Expecting integer value for asset holding")
+	//}
+	//fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+	//
+	//// Write the state to the ledger
+	//err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	//if err != nil {
+	//	return shim.Error(err.Error())
+	//}
+	//
+	//err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	//if err != nil {
+	//	return shim.Error(err.Error())
+	//}
+	//
+	//return shim.Success(nil)
+
+	fmt.Println("Init")
+	var err error
+	// Initialize the chaincode
+	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+	// Write the state to the ledger
+	err = ctx.GetStub().PutState(A, []byte(strconv.Itoa(Aval)))
+	if err != nil {
+		return err
+	}
+
+	err = ctx.GetStub().PutState(B, []byte(strconv.Itoa(Bval)))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Transaction makes payment of X units from A to B
+func (t *SmartContract) Invoke(ctx contractapi.TransactionContextInterface, A, B string, X int) error {
+	var err error
+	var Aval int
+	var Bval int
+	// Get the state from the ledger
+	Avalbytes, err := ctx.GetStub().GetState(A)
+	if err != nil {
+		return fmt.Errorf("Failed to get state")
+	}
+	if Avalbytes == nil {
+		return fmt.Errorf("Entity not found")
+	}
+	Aval, _ = strconv.Atoi(string(Avalbytes))
+
+	Bvalbytes, err := ctx.GetStub().GetState(B)
+	if err != nil {
+		return fmt.Errorf("Failed to get state")
+	}
+	if Bvalbytes == nil {
+		return fmt.Errorf("Entity not found")
+	}
+	Bval, _ = strconv.Atoi(string(Bvalbytes))
+
+	// Perform the execution
+	Aval = Aval - X
+	Bval = Bval + X
+	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+
+	// Write the state back to the ledger
+	err = ctx.GetStub().PutState(A, []byte(strconv.Itoa(Aval)))
+	if err != nil {
+		return err
+	}
+
+	err = ctx.GetStub().PutState(B, []byte(strconv.Itoa(Bval)))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete  an entity from state
+func (t *SmartContract) Delete(ctx contractapi.TransactionContextInterface, A string) error {
+
+	// Delete the key from the state in ledger
+	err := ctx.GetStub().DelState(A)
+	if err != nil {
+		return fmt.Errorf("Failed to delete state")
+	}
+
+	return nil
+}
+
+// Query callback representing the query of a chaincode
+func (t *SmartContract) Query(ctx contractapi.TransactionContextInterface, A string) (string, error) {
+	var err error
+	// Get the state from the ledger
+	Avalbytes, err := ctx.GetStub().GetState(A)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
+		return "", errors.New(jsonResp)
+	}
+
+	if Avalbytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
+		return "", errors.New(jsonResp)
+	}
+
+	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+	return string(Avalbytes), nil
+}
+
+func main() {
+	cc, err := contractapi.NewChaincode(new(SmartContract))
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := cc.Start(); err != nil {
+		fmt.Printf("Error starting ABstore chaincode: %s", err)
+	}
+}
