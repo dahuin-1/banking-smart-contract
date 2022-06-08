388026b83839f493ed03641676617d0e15090508
dahuin-1 <dawhite01@naver.com>
2022-06-08 14:36:09 +0900

    edit

--- banking/go/banking.go
+++ banking/go/banking.go
@@ -4,239 +4,250 @@ import (
 	"encoding/json"
 	"fmt"
 
-	//"github.com/hyperledger/fabric/core/chaincode/shim"
-	//sc "github.com/hyperledger/fabric/protos/peer"
-	"github.com/hyperledger/fabric-chaincode-go/shim"
-	sc "github.com/hyperledger/fabric-protos-go/peer"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+	//"github.com/hyperledger/fabric-chaincode-go/shim"
+	//"github.com/hyperledger/fabric-protos-go/peer"
 	"strconv"
 )
 
-type SmartContract struct {
+type Chaincode struct {
 }
 
 type Account struct {
-	Owner string `json:"owner"`
-	//Token   string `json:"token"`
-	Balance int `json:"balance"`
+	OwnerID string `json:"owner_id"`
+	Balance int64  `json:"balance"`
 }
 
-func (t *SmartContract) Init() sc.Response {
-
-	//fmt.Println("Init")
-	//var err error
-	//// Initialize the chaincode
-	//fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-	//err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	//if err != nil {
-	//	return shim.Error("")
-	//}
-	//
-	//err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	//if err != nil {
-	//	return shim.Error("")
-	//}
-	//
-	//return shim.Success(nil)
-	//fmt.Println("Init")
-	//_, args := stub.GetFunctionAndParameters()
-	//var A, B string           // Entities
-	//var Atoken, Btoken string // Token name holdings
-	//var Aval, Bval int        // Asset holdings
-	//var err error
-	//
-	//if len(args) != 4 {
-	//	return shim.Error("Incorrect number of arguments. Expecting 4")
-	//}
-	//
-	//// Initialize the chaincode
-	//A = args[0]
-	//Atoken = args[1]
-	//Aval, err = strconv.Atoi(args[2])
-	//if err != nil {
-	//	return shim.Error("Expecting integer value for asset holding")
-	//}
-	//B = args[3]
-	//Btoken = args[4]
-	//Bval, err = strconv.Atoi(args[5])
-	//if err != nil {
-	//	return shim.Error("Expecting integer value for asset holding")
-	//}
-	//fmt.Printf("%s has %s !! val = %d\n, %s has %s !! val = %d", A, Atoken, Aval, B, Btoken, Bval)
-	//
-	//// Write the state to the ledger
-	//err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	//if err != nil {
-	//	return shim.Error(err.Error())
-	//}
-	//
-	//err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	//if err != nil {
-	//	return shim.Error(err.Error())
-	//}
-
+func (c *Chaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
 	return shim.Success(nil)
 }
 
-// Transaction makes payment of X units from A to B
-func (t *SmartContract) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
+func (c *Chaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	function, args := stub.GetFunctionAndParameters()
 	if function == "transfer" {
-		// Make payment of X units from A to B
-		return t.transfer(stub, args)
+		return chaincode.transfer(stub, args)
 	} else if function == "deleteAccount" {
-		// Deletes an entity from its state
-		return t.deleteAccount(stub, args)
-	} else if function == "query" {
-		return t.query(stub, args)
+		return chaincode.deleteAccount(stub, args)
+	} else if function == "getAccount" {
+		return chaincode.getAccount(stub, args)
 	} else if function == "createAccount" {
-		return t.createAccount(stub, args)
+		return chaincode.createAccount(stub, args)
+	} else if function == "deposit" {
+		return chaincode.deposit(stub, args)
+	} else if function == "withdrawal" {
+		return chaincode.withdrawal(stub, args)
 	}
-
-	return shim.Error("Invalid invoke function name. Expecting \"invoke\" \"delete\" \"query\"")
+	return shim.Error("Invalid function name")
 }
 
-func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Response {
-	accounts := []Account{
-		Account{Owner: "Tomoko", Balance: 200},
-		Account{Owner: "Brad", Balance: 100},
-		Account{Owner: "Ji Soo", Balance: 500},
-		Account{Owner: "Max", Balance: 1000},
-		Account{Owner: "Amy", Balance: 300},
-		Account{Owner: "Michel", Balance: 400},
-		Account{Owner: "Ann", Balance: 600},
-		Account{Owner: "Pari", Balance: 150},
-		Account{Owner: "Valeria", Balance: 700},
-		Account{Owner: "Shotaro", Balance: 800},
+func (c *Chaincode) createAccount(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	//(a,100)
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
+	var numBalance int64
+	key := args[0]
 
-	i := 0
-	for i < len(accounts) {
-		fmt.Println("i is ", i)
-		accountAsBytes, _ := json.Marshal(accounts[i])
-		owner := accounts[i].Owner
-		err := APIstub.PutState(owner, accountAsBytes)
-		if err != nil {
-			return sc.Response{}
-		}
-		fmt.Println("Added", accounts[i])
-		i = i + 1
+	accountAsBytes, err := stub.GetState(key)
+	if err != nil {
+		return shim.Error("Failed to get OwnerID: " + err.Error())
+	} else if accountAsBytes != nil {
+		fmt.Println("This ownerID already exists: " + key)
+		return shim.Error("This account already exists: " + key)
 	}
-
-	return shim.Success(nil)
-}
-
-func (s *SmartContract) createAccount(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
-
-	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3")
-	}
-	strBalance, err := strconv.Atoi(args[2])
+	numBalance, err = strBalToInt(args[1])
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-	var account = Account{Owner: args[1], Balance: strBalance}
-
-	accountAsBytes, _ := json.Marshal(account)
-	err = APIstub.PutState(args[0], accountAsBytes)
+	var account = Account{OwnerID: args[0], Balance: numBalance}
+	accountAsBytes, err = json.Marshal(account)
 	if err != nil {
-		return sc.Response{}
+		return shim.Error(err.Error())
 	}
-
-	return shim.Success(nil)
+	err = stub.PutState(key, accountAsBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(accountAsBytes)
 }
 
-func (t *SmartContract) transfer(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+func (c *Chaincode) transfer(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	//(a,b,10)
-	var err error
-	var sender, receiver string // Entities
-	var remittance int
-	var Sval, Rval int
+	if len(args) != 3 {
+		return shim.Error("Incorrect number of arguments. Expecting 3")
+	}
+	var sender, receiver string
+	var remittance int64
+	var senderAccount, receiverAccount Account
 
 	sender = args[0]
 	receiver = args[1]
 
+	if sender == receiver {
+		return shim.Error("receiver and sender cannot be the same")
+	}
 	senderAccountAsBytes, err := stub.GetState(sender)
 	if err != nil {
-		return shim.Error("Failed to get state")
+		return shim.Error(err.Error())
+	} else if senderAccountAsBytes == nil {
+		return shim.Error("SenderAccount not found")
 	}
-	if senderAccountAsBytes == nil {
-		return shim.Error("Entity not found")
+	err = json.Unmarshal(senderAccountAsBytes, &senderAccount)
+	if err != nil {
+		return shim.Error(err.Error())
 	}
-	Sval, _ = strconv.Atoi(string(senderAccountAsBytes))
 
 	receiverAccountAsBytes, err := stub.GetState(receiver)
 	if err != nil {
-		return shim.Error("Failed to get state")
+		return shim.Error(err.Error())
+	} else if receiverAccountAsBytes == nil {
+		return shim.Error("ReceieverAccount not found")
 	}
-	if receiverAccountAsBytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Rval, _ = strconv.Atoi(string(receiverAccountAsBytes))
-
-	//	token = args[2]
-	remittance, err = strconv.Atoi(args[2])
-	if err != nil {
-		return shim.Error("Invalid transaction amount, expecting a integer value")
-	}
-
-	Sval = Sval - remittance
-	Rval = Rval + remittance
-	fmt.Printf("Sval = %d, Rval = %d\n", Sval, Rval)
-
-	// Write the state back to the ledger
-	err = stub.PutState(sender, []byte(strconv.Itoa(Sval)))
+	err = json.Unmarshal(receiverAccountAsBytes, &receiverAccount)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	err = stub.PutState(receiver, []byte(strconv.Itoa(Rval)))
+	remittance, err = strBalToInt(args[2])
 	if err != nil {
 		return shim.Error(err.Error())
 	}
+	if remittance < 0 {
+		return shim.Error("remittance cannot be negative")
+	} else if remittance > senderAccount.Balance {
+		return shim.Error("remittance must be less than the balance")
+	}
+	senderAccount.Balance = senderAccount.Balance - remittance
+	receiverAccount.Balance = receiverAccount.Balance + remittance
 
+	senderAccountAsBytes, err = json.Marshal(senderAccount)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	receiverAccountAsBytes, err = json.Marshal(receiverAccount)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	err = stub.PutState(sender, senderAccountAsBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	err = stub.PutState(receiver, receiverAccountAsBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(senderAccountAsBytes)
+}
+
+func (c *Chaincode) deleteAccount(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+	var deletedAccount string
+	deletedAccount = args[0]
+	err := stub.DelState(deletedAccount)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
 	return shim.Success(nil)
 }
 
-// Delete  an entity from state
-func (t *SmartContract) deleteAccount(stub shim.ChaincodeStubInterface, args []string) sc.Response {
-	A := args[0]
-	// Delete the key from the state in ledger
-	err := stub.DelState(A)
-	if err != nil {
-		return shim.Error("Failed to delete state")
+func (c *Chaincode) getAccount(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	//(a)
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
-
-	return shim.Success(nil)
+	key := args[0]
+	accountAsBytes, err := stub.GetState(key)
+	if err != nil {
+		return shim.Error(err.Error())
+	} else if accountAsBytes == nil {
+		return shim.Error("This account dose not exists: " + key)
+	}
+	return shim.Success(accountAsBytes)
 }
 
-// Query callback representing the query of a chaincode
-func (t *SmartContract) query(stub shim.ChaincodeStubInterface, args []string) sc.Response {
-	var err error
-	A := args[0]
-	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
+func (c *Chaincode) deposit(stub shim.ChaincodeStubInterface, args []string) peer.Response { //입금
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+	var amount int64
+	var targetAccount Account
+
+	key := args[0]
+	accountAsBytes, err := stub.GetState(key)
 	if err != nil {
-
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return shim.Error(jsonResp)
+		return shim.Error(err.Error())
+	} else if accountAsBytes == nil {
+		return shim.Error("This account dose not exists: " + key)
 	}
-
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-		return shim.Error(jsonResp)
+	err = json.Unmarshal(accountAsBytes, &targetAccount)
+	if err != nil {
+		return shim.Error(err.Error())
 	}
+	amount, err = strBalToInt(args[1])
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	targetAccount.Balance = targetAccount.Balance + amount
+	accountAsBytes, err = json.Marshal(targetAccount)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	err = stub.PutState(key, accountAsBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(accountAsBytes)
+}
 
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	return shim.Success(nil)
+func (c *Chaincode) withdrawal(stub shim.ChaincodeStubInterface, args []string) peer.Response { //출금
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+	var amount int64
+	var targetAccount Account
+	key := args[0]
+	accountAsBytes, err := stub.GetState(key)
+	if err != nil {
+		return shim.Error(err.Error())
+	} else if accountAsBytes == nil {
+		return shim.Error("This account dose not exists: " + key)
+	}
+	err = json.Unmarshal(accountAsBytes, &targetAccount)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	amount, err = strBalToInt(args[1])
+	if err != nil {
+		return shim.Error(err.Error())
+	} else if amount > targetAccount.Balance {
+		return shim.Error("amount must be less than the balance")
+	}
+	targetAccount.Balance = targetAccount.Balance - amount
+	accountAsBytes, err = json.Marshal(targetAccount)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	err = stub.PutState(key, accountAsBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(accountAsBytes)
+}
+
+func strBalToInt(strBal string) (iBal int64, err error) {
+	iBal, err = strconv.ParseInt(strBal, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return iBal, err
 }
 
 func main() {
-	err := shim.Start(new(SmartContract))
+	err := shim.Start(new(Chaincode))
 	if err != nil {
 		panic(err.Error())
 	}
-	//if err := cc.Start(); err != nil {
-	//	fmt.Printf("Error starting ABstore chaincode: %s", err)
-	//}
 }
